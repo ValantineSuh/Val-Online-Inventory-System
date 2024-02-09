171ce30bdd4a7267ac79948e79d62b1a2048e4cf
Awambeng <awambengrodrick@gmail.com>
2024-02-10 00:09:18 +0100

    added: implement dashboard

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Online inventory system'
+@app.route('/main')
+def main():
+    # return 'Online inventory system'
+    return render_template("main.html")
 
 app.run(host='0.0.0.0', port=5000)
